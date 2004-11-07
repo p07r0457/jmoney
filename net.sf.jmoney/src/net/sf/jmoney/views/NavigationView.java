403497e405ddcf6854778329763cbd795491da95
jgyger <jgyger@b97006a3-7e3d-0410-986d-ea81e883d316>
2004-11-07 20:00:54 +0000

    Expand all accounts when refreshing the view.
    git-svn-id: https://jmoney.svn.sf.net/svnroot/jmoney/trunk@59 b97006a3-7e3d-0410-986d-ea81e883d316

--- net.sf.jmoney/src/net/sf/jmoney/views/NavigationView.java
+++ net.sf.jmoney/src/net/sf/jmoney/views/NavigationView.java
@@ -335,6 +335,7 @@ public class NavigationView extends ViewPart {
         Display.getDefault().syncExec( new Runnable() {
             public void run() {
                 viewer.refresh(accountsRootNode, false);
+                viewer.expandAll();
             }
         });
 	}
