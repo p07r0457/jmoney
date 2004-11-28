70201660c564a59330a8f0efe35e100e58cb6d82
jgyger <jgyger@b97006a3-7e3d-0410-986d-ea81e883d316>
2004-11-28 16:09:16 +0000

    - Merge with Olivier's implementation - First attempt to implement cell editing
    git-svn-id: https://jmoney.svn.sf.net/svnroot/jmoney/trunk@112 b97006a3-7e3d-0410-986d-ea81e883d316

--- net.sf.jmoney.ui/src/net/sf/jmoney/ui/internal/pages/account/capital/EntriesSection.java
+++ net.sf.jmoney.ui/src/net/sf/jmoney/ui/internal/pages/account/capital/EntriesSection.java
@@ -27,6 +27,7 @@ import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -34,9 +35,11 @@ import java.util.Vector;
 
 import net.sf.jmoney.model2.CapitalAccount;
 import net.sf.jmoney.model2.Entry;
-import net.sf.jmoney.model2.Session;
 
+import org.eclipse.jface.viewers.CellEditor;
 import org.eclipse.jface.viewers.ColumnWeightData;
+import org.eclipse.jface.viewers.ComboBoxCellEditor;
+import org.eclipse.jface.viewers.ICellModifier;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredContentProvider;
 import org.eclipse.jface.viewers.ITableLabelProvider;
@@ -44,6 +47,7 @@ import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.TableLayout;
 import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TextCellEditor;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerSorter;
 import org.eclipse.swt.SWT;
@@ -51,10 +55,8 @@ import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.TabItem;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
-import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.ui.forms.SectionPart;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.forms.widgets.Section;
@@ -66,23 +68,25 @@ import org.eclipse.ui.forms.widgets.Section;
  */
 public class EntriesSection extends SectionPart {
 
-	protected EntriesPage fPage;
-    protected Table fTable; 
-	protected TableViewer fViewer;
+    protected static final String[] PROPERTIES = { "check", "date", "valuta", "description", "debit", "credit", "balance"};
+
+    protected EntriesPage fPage;
+    protected Table fTable;
+    protected TableViewer fViewer;
 
     public EntriesSection(EntriesPage page, Composite parent) {
         super(parent, page.getManagedForm().getToolkit(), Section.TITLE_BAR);
         getSection().setText("All Entries");
-		fPage = page;
-		createClient(page.getManagedForm().getToolkit());
-	}
+        fPage = page;
+        createClient(page.getManagedForm().getToolkit());
+    }
 
-	public void refresh() {
+    public void refresh() {
         fViewer.refresh();
         super.refresh();
     }
 
-	protected void createClient(FormToolkit toolkit) {
+    protected void createClient(FormToolkit toolkit) {
         Composite container = toolkit.createComposite(getSection());
 
         GridLayout layout = new GridLayout();
@@ -97,192 +101,200 @@ public class EntriesSection extends SectionPart {
         refresh();
     }
 
-	protected void createTable(Composite container, FormToolkit toolkit) {
+    protected void createTable(Composite container, FormToolkit toolkit) {
         fTable = toolkit.createTable(container, SWT.FULL_SELECTION);
         GridData gd = new GridData(GridData.FILL_BOTH);
         gd.heightHint = 100;
         fTable.setLayoutData(gd);
 
-        TableColumn col1 = new TableColumn(fTable, SWT.NULL);
-        col1.setText("Check");
-        TableColumn col2 = new TableColumn(fTable, SWT.NULL);
-        col2.setText("Date");
-        TableColumn col3 = new TableColumn(fTable, SWT.NULL);
-        col3.setText("Description");
-        TableColumn col4 = new TableColumn(fTable, SWT.RIGHT);
-        col4.setText("Debit");
-        TableColumn col5 = new TableColumn(fTable, SWT.RIGHT);
-        col5.setText("Credit");
-        TableColumn col6 = new TableColumn(fTable, SWT.RIGHT);
-        col6.setText("Balance");
-
+        TableColumn col;
         TableLayout tlayout = new TableLayout();
-        tlayout.addColumnData(new ColumnWeightData(50, 50));
-        tlayout.addColumnData(new ColumnWeightData(50, 70));
-        tlayout.addColumnData(new ColumnWeightData(50, 300));
-        tlayout.addColumnData(new ColumnWeightData(50, 70));
-        tlayout.addColumnData(new ColumnWeightData(50, 70));
-        tlayout.addColumnData(new ColumnWeightData(50, 70));
-        fTable.setLayout(tlayout);
 
+        col = new TableColumn(fTable, SWT.NULL);
+        col.setText("Check");
+        tlayout.addColumnData(new ColumnWeightData(50, 50));
+
+        col = new TableColumn(fTable, SWT.NULL);
+        col.setText("Date");
+        tlayout.addColumnData(new ColumnWeightData(50, 70));
+
+        col = new TableColumn(fTable, SWT.NULL);
+        col.setText("Valuta");
+        tlayout.addColumnData(new ColumnWeightData(50, 70));
+
+        col = new TableColumn(fTable, SWT.NULL);
+        col.setText("Description");
+        tlayout.addColumnData(new ColumnWeightData(50, 300));
+
+        col = new TableColumn(fTable, SWT.RIGHT);
+        col.setText("Debit");
+        tlayout.addColumnData(new ColumnWeightData(50, 70));
+
+        col = new TableColumn(fTable, SWT.RIGHT);
+        col.setText("Credit");
+        tlayout.addColumnData(new ColumnWeightData(50, 70));
+
+        col = new TableColumn(fTable, SWT.RIGHT);
+        col.setText("Balance");
+        tlayout.addColumnData(new ColumnWeightData(50, 70));
+
+        fTable.setLayout(tlayout);
         fTable.setHeaderVisible(true);
         fTable.setLinesVisible(true);
     }
 
-	protected void createTableViewer() {
+    protected void createTableViewer() {
         fViewer = new TableViewer(fTable);
         fViewer.setContentProvider(new ContentProvider());
-        fViewer.setInput(fPage.getAccount());
+        fViewer.setLabelProvider(new EntryLabelProvider());
+        fViewer.setSorter(new EntrySorter());
+
+        fViewer.setColumnProperties(PROPERTIES);
+        fViewer.setCellEditors(createCellEditors());
+        fViewer.setCellModifier(new CellModifier());
+
         fViewer.addSelectionChangedListener(new ISelectionChangedListener() {
             public void selectionChanged(SelectionChangedEvent event) {
                 handleSelectionChanged();
             }
         });
-        
-        fViewer.setLabelProvider(new MyLabelProvider(fViewer));
-        fViewer.setSorter(new mySorter());
+
+        fViewer.setInput(fPage.getAccount());
     }
-	
-	protected void handleSelectionChanged() {
-	    // TODO
-	}
 
-	class ContentProvider implements IStructuredContentProvider {
+    // TODO This is not implemnted properly yet.
+    protected CellEditor[] createCellEditors() {
+        CellEditor[] result = new CellEditor[7];
+        result[0] = new ComboBoxCellEditor(fTable, new String[] { "a", "b", "c"}); // check
+        result[1] = new TextCellEditor(fTable); // date
+        result[2] = new TextCellEditor(fTable); // valuta
+        result[3] = new TextCellEditor(fTable); // description
+        result[4] = new TextCellEditor(fTable); // debit
+        result[5] = new TextCellEditor(fTable); // credit
+        result[6] = new TextCellEditor(fTable); // balance
+        return result;
+    }
 
-	    public void dispose() {
+    protected void handleSelectionChanged() {
+        // TODO
+    }
+
+    class ContentProvider implements IStructuredContentProvider {
+        public void dispose() {
             // TODO Auto-generated method stub
         }
-
         public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
             // TODO Auto-generated method stub
         }
-
-        /* 
+        /** 
          * Returns the elements the table has to display. 
          * We should give the entry back with other informations the display
          * needs, for example the saldo.
          */
         public Object[] getElements(Object parent) {
             CapitalAccount account = (CapitalAccount) parent;
+            List entries = new LinkedList();
 
-            List sortedEntries = getEntriesFromAccount(account);
-            sortedEntries = sortChronogicalyEntries(sortedEntries);
+            Iterator it = account.getEntriesIterator(account.getSession());
+            while (it.hasNext()) {
+                entries.add(it.next());
+            }
 
-            // TODO Iterator workaround
-            Vector entries = new Vector();
+            Collections.sort(entries, new Comparator() {
+                public int compare(Object a, Object b) {
+                    Date d1 = ((Entry) a).getTransaction().getDate();
+                    Date d2 = ((Entry) b).getTransaction().getDate();
+                    // TODO Taken from EntryComparator as a quick fix
+                    if (d1 == null && d2 == null) return 0;
+                    if (d1 == null) return 1;
+                    if (d2 == null) return -1;
+                    return d1.compareTo(d2);
+                }
+            });
+
+            Vector d_entries = new Vector();
             long saldo = 0;
-            
-            for (int i = 0; i<sortedEntries.size(); i++) {
-                Entry e = (Entry) sortedEntries.get(i);
-                
+            for (int i = 0; i < entries.size(); i++) {
+                Entry e = (Entry) entries.get(i);
                 saldo = saldo + e.getAmount();
-                EntryWithInformationForDisplay ewifd = new EntryWithInformationForDisplay (e, saldo);
-                
-                entries.add(ewifd);
+                d_entries.add(new DisplayableEntry(e, saldo));
             }
-            
-            return entries.toArray();
+
+            return d_entries.toArray();
         }
-
-	}
-	
-    // Definition of the LabelProvider, or "how we display the information
-    // in the table".
-	private final class MyLabelProvider extends LabelProvider implements ITableLabelProvider {
-	    
-	    private final DateFormat df;
-	    private final NumberFormat nf;
-	    private final TableViewer attachedViewer;
-	    
-	    public MyLabelProvider(TableViewer attachedViewer) {
-	        df = new SimpleDateFormat("dd.MM.yyyy");
-	        nf = DecimalFormat.getCurrencyInstance();
-	        this.attachedViewer = attachedViewer;
-	    }
-	    
-	    public String getColumnText(Object o, int colnr) {
-	        if (o instanceof EntryWithInformationForDisplay) {
-	            Entry e = ((EntryWithInformationForDisplay) o).entry;
-	            long saldo = ((EntryWithInformationForDisplay) o).saldo;
-	            
-	            if (colnr == 1 ) {  
-	                return df.format(e.getTransaction().getDate());
-	            } else if (colnr == 2) { 
-	                return e.getDescription();
-	            } else if (colnr == 3) {  
-	                if (e.getAmount() > 0) 
-	                   return nf.format(e.getAmount() / 100); 
-	                   else return new String ();
-	            } else if (colnr == 4) {
-	                if (e.getAmount() < 0) 
-	                   return nf.format( - e.getAmount() / 100); 
-	                   else return new String ();
-	            } else if (colnr == 5) {
-	                return nf.format( saldo / 100);
-	            }
-
-	        }
-	        return new String();
-	    }
-	    
-	    public Image getColumnImage(Object o, int colnr) {return null;}
-
-	}
-	
-	private final class mySorter extends ViewerSorter {
-	    public int compare (Viewer notused, Object o1, Object o2) {
-	        if (o1 instanceof EntryWithInformationForDisplay && o2 instanceof EntryWithInformationForDisplay) {
-	            return ((EntryWithInformationForDisplay) o1).entry.getTransaction().getDate()
-	               .compareTo(((EntryWithInformationForDisplay) o2).entry.getTransaction().getDate());
-	        } else {
-	            return super.compare(notused, o1, o2);
-	        }
-	    }
-	}
-	
-	private final class EntryWithInformationForDisplay {
-	    private Entry entry;
-	    private long saldo;
-	    
-	    public EntryWithInformationForDisplay (Entry entry, long saldo) {
-	        this.entry = entry;
-	        this.saldo = saldo;
-	    }
-	}
-	
-	/**
-	 * return the list of the entries of an account, chronlogicaly ordered.
-	 * @param session
-	 * @param a
-	 * @return
-	 */
-	private static List sortChronogicalyEntries(List entries) {
-	    
-
-        Collections.sort(entries, new Comparator() {
-            public int compare(Object a, Object b) {
-                return ((Entry) a).getTransaction().getDate().compareTo(
-                        ((Entry) b).getTransaction().getDate());
-            }
-        });
-
-        return entries;
     }
-	
-	
-	private static List getEntriesFromAccount (CapitalAccount a) {
-	    
-        List entries = new LinkedList();
-        
-        Iterator it = a.getEntriesIterator(a.getSession());
-        while (it.hasNext()) {
-            entries.add(it.next());
+
+    class EntryLabelProvider extends LabelProvider implements ITableLabelProvider {
+        // TODO Use formatting capabilities of IPropertyAccessor
+        // as soon as they are available.
+        protected DateFormat df = new SimpleDateFormat("dd.MM.yyyy");
+        protected NumberFormat nf = DecimalFormat.getCurrencyInstance();
+        public String getColumnText(Object obj, int index) {
+            DisplayableEntry de = (DisplayableEntry) obj;
+            switch (index) {
+            case 0: // check
+                String s = de.entry.getCheck();
+                return s == null ? "" : s;
+            case 1: // date
+                Date d = de.entry.getTransaction().getDate();
+                return d == null ? "" : df.format(d);
+            case 2: // valuta
+                d = de.entry.getValuta();
+                return d == null ? "" : df.format(d);
+            case 3: // description
+                s = de.entry.getDescription() + "";
+                return s == null ? "" : s;
+            case 4: // income
+                return de.entry.getAmount() >= 0 ? nf.format(de.entry.getAmount() / 100) : "";
+            case 5: // expense
+                return de.entry.getAmount() < 0 ? nf.format(-de.entry.getAmount() / 100) : "";
+            case 6: // balance
+                return nf.format(de.balance / 100);
+            }
+            return ""; //$NON-NLS-1$
         }
+        public Image getColumnImage(Object obj, int index) {
+            return null;
+        }
+    }
 
-        return entries;
-	}
-	
+    class EntrySorter extends ViewerSorter {
+        public int compare(Viewer notused, Object o1, Object o2) {
+            if (o1 instanceof DisplayableEntry && o2 instanceof DisplayableEntry) {
+                Date d1 = ((DisplayableEntry) o1).entry.getTransaction().getDate();
+                Date d2 = ((DisplayableEntry) o2).entry.getTransaction().getDate();
+                // TODO Taken from EntryComparator as a quick fix
+                if (d1 == null && d2 == null) return 0;
+                if (d1 == null) return 1;
+                if (d2 == null) return -1;
+                return d1.compareTo(d2);
+            } else {
+                return super.compare(notused, o1, o2);
+            }
+        }
+    }
 
+    class DisplayableEntry {
+        private Entry entry;
+        private long balance;
+
+        public DisplayableEntry(Entry entry, long saldo) {
+            this.entry = entry;
+            this.balance = saldo;
+        }
+    }
+
+    class CellModifier implements ICellModifier {
+        public boolean canModify(Object element, String property) {
+            return true;
+        }
+        public Object getValue(Object element, String property) {
+            if (property.equals("check")) { return new Integer(0); }
+            return "";
+        }
+        public void modify(Object element, String property, Object value) {
+        }
+    }
 
 }
 
