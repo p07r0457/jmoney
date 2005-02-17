1ce521d9a78492a5437fa491d3bdb0e089b964eb
westbury <westbury@b97006a3-7e3d-0410-986d-ea81e883d316>
2005-02-17 02:49:04 +0000

    move the account entries list page into the core net.sf.jmoney plug-in
    git-svn-id: https://jmoney.svn.sf.net/svnroot/jmoney/trunk@234 b97006a3-7e3d-0410-986d-ea81e883d316

---
+++ net.sf.jmoney/src/net/sf/jmoney/pages/entries/IDisplayableItem.java
@@ -0,0 +1,68 @@
+/*
+ * Created on Feb 2, 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package net.sf.jmoney.pages.entries;
+
+import net.sf.jmoney.model2.Entry;
+import net.sf.jmoney.model2.Transaction;
+
+/**
+ * @author Nigel
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public interface IDisplayableItem {
+    
+	/**
+	 * If transaction fields are to be shown (i.e. if this
+	 * is a DisplayableTransaction row), return the Transaction.
+	 */
+	Transaction getTransactionForTransactionFields();
+
+	/**
+	 * If fields for capital account are to be shown,
+	 * return this...
+	 * @return
+	 */
+	Entry getEntryForAccountFields();
+	
+	Entry getEntryForOtherFields();
+	
+	//Entry getEntryForDualPurposeFields();
+	
+	long getAmount();
+
+	/**
+	 *  
+	 * 
+	 * 
+	 * @return true if the transaction fields (the date and any
+	 * 		extension properties added to the transaction) are to 
+	 * 		be blank on this row
+	 */
+	boolean blankTransactionFields();
+	
+	/**
+	 * 
+	 * 
+	 * 
+	 * @return true if this row causes the balance
+	 * 		(as shown in the balance column) to be a different
+	 * 		value from the previous balance
+	 */
+	boolean isBalanceAffected();
+	
+	/**
+	 * This method is valid only if isBalanceAffected
+	 * returns true.
+	 * 
+	 * @return
+	 */
+	long getBalance();
+	
+	
+}
