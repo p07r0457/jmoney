3d4092a4d8e3885e96d465f97de160f5398783c7
westbury <westbury@b97006a3-7e3d-0410-986d-ea81e883d316>
2005-02-17 03:01:51 +0000

    move the account entries list page into the core net.sf.jmoney plug-in
    git-svn-id: https://jmoney.svn.sf.net/svnroot/jmoney/trunk@235 b97006a3-7e3d-0410-986d-ea81e883d316

--- net.sf.jmoney.ui/src/net/sf/jmoney/ui/internal/JMoneyUIPlugin.java
+++
@@ -1,109 +0,0 @@
-/*
- *
- *  JMoney - A Personal Finance Manager
- *  Copyright (c) 2004 Johann Gyger <jgyger@users.sf.net>
- *
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- */
-package net.sf.jmoney.ui.internal;
-
-import java.net.MalformedURLException;import java.net.URL;import java.util.MissingResourceException;import java.util.ResourceBundle;import org.eclipse.core.runtime.Platform;
-import org.eclipse.jface.resource.ImageDescriptor;import org.eclipse.swt.graphics.Color;import org.eclipse.swt.widgets.Display;import org.eclipse.ui.IWorkbench;import org.eclipse.ui.IWorkbenchPage;import org.eclipse.ui.IWorkbenchWindow;import org.eclipse.ui.PlatformUI;import org.eclipse.ui.plugin.AbstractUIPlugin;import org.osgi.framework.BundleContext;
-
-/**
- * The main plugin class to be used in the desktop.
- */
-public class JMoneyUIPlugin extends AbstractUIPlugin {
-
-    public static final boolean DEBUG = "true".equalsIgnoreCase(Platform.getDebugOption("net.sf.jmoney.ui/debug"));
-
-	private static JMoneyUIPlugin plugin;
-
-	private ResourceBundle resourceBundle;
-	private Color yellow;	private Color green;	
-	/**
-	 * The constructor.
-	 */
-	public JMoneyUIPlugin() {
-		super();
-		plugin = this;
-		try {
-			resourceBundle = ResourceBundle.getBundle("net.sf.jmoney.ui.internal.Language");
-		} catch (MissingResourceException x) {
-			resourceBundle = null;
-		}
-	}
-
-	/**
-	 * Returns the shared instance.
-	 */
-	public static JMoneyUIPlugin getDefault() {
-		return plugin;
-	}
-
-	/**
-	 * Returns the string from the plugin's resource bundle,
-	 * or 'key' if not found.
-	 */
-	public static String getResourceString(String key) {
-		ResourceBundle bundle = JMoneyUIPlugin.getDefault().getResourceBundle();
-		try {
-			return (bundle != null) ? bundle.getString(key) : key;
-		} catch (MissingResourceException e) {
-			return key;
-		}
-	}
-
-	/**
-	 * @return Active workbench window
-	 */
-    public static IWorkbenchWindow getActiveWorkbenchWindow() {
-        IWorkbench workbench = PlatformUI.getWorkbench();
-        if (workbench == null) return null;
-        return workbench.getActiveWorkbenchWindow();
-    }
-
-    /**
-     * @return Active page
-     */
-    public static IWorkbenchPage getActivePage() {
-        IWorkbenchWindow window = getActiveWorkbenchWindow();
-        if (window == null) return null;
-        return window.getActivePage();
-    }
-
-	/**
-	 * This method is called upon plug-in activation
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		Display display = getActiveWorkbenchWindow().getShell().getDisplay();				// A useful site for picking colors: http://www.drpeterjones.com/colorcalc/		yellow = new Color(display, 255, 255, 150);		green = new Color(display, 200, 255, 200);	}
-
-	/**
-	 * This method is called when the plug-in is stopped
-	 */
-	public void stop(BundleContext context) throws Exception {		yellow.dispose();		green.dispose();		
-		super.stop(context);	}
-
-	/**
-	 * Returns the plugin's resource bundle,
-	 */
-	public ResourceBundle getResourceBundle() {
-		return resourceBundle;
-	}	public static ImageDescriptor createImageDescriptor(String name) {		String iconPath = "";		try {			URL installURL = getDefault().getBundle().getEntry("/");			URL url = new URL(installURL, iconPath + name);			return ImageDescriptor.createFromURL(url);		} catch (MalformedURLException e) {			// should not happen			return ImageDescriptor.getMissingImageDescriptor();		}	}	/**	 * @return	 */	public Color getGreenColor() {		return green;	}	/**	 * @return	 */	public Color getYellowColor() {		return yellow;	}
-
-}
--- net.sf.jmoney.ui/src/net/sf/jmoney/ui/internal/pages/account/bank/PropertiesSection.java
+++
@@ -1,102 +0,0 @@
-/*
- *
- *  JMoney - A Personal Finance Manager
- *  Copyright (c) 2004 Johann Gyger <jgyger@users.sf.net>
- *
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- */
-package net.sf.jmoney.ui.internal.pages.account.bank;
-
-import net.sf.jmoney.model2.BankAccount;
-
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Text;
-import org.eclipse.ui.forms.SectionPart;
-import org.eclipse.ui.forms.widgets.FormToolkit;
-import org.eclipse.ui.forms.widgets.Section;
-
-/**
- * TODO
- * 
- * @author Johann Gyger
- */
-public class PropertiesSection extends SectionPart {
-
-	protected PropertiesPage fPage;
-    protected Text fName; 
-    protected Text fCurrency; 
-    protected Text fBank; 
-    protected Text fAccountNumber; 
-    protected Text fStartBalance; 
-    protected Text fMinBalance; 
-    protected Text fComment; 
-
-	public PropertiesSection(PropertiesPage page, Composite parent) {
-        super(parent, page.getManagedForm().getToolkit(), Section.DESCRIPTION | Section.TITLE_BAR);
-        fPage = page;
-        getSection().setText("Account Properties");
-        getSection().setDescription("Specify general properties of this capital account.");
-        createClient(page.getManagedForm().getToolkit());
-    }
-
-	protected void createClient(FormToolkit toolkit) {
-        Composite container = toolkit.createComposite(getSection());
-
-        GridLayout layout = new GridLayout();
-        layout.numColumns = 2;
-        container.setLayout(layout);
-
-        toolkit.createLabel(container, "Name:");
-        fName = toolkit.createText(container, "");
-        fName.setTextLimit(50);
-        toolkit.createLabel(container, "Currency:");
-        fCurrency = toolkit.createText(container, "");
-        toolkit.createLabel(container, "Bank:");
-        fBank = toolkit.createText(container, "");
-        toolkit.createLabel(container, "Account Number:");
-        fAccountNumber = toolkit.createText(container, "");
-        toolkit.createLabel(container, "Start Balance:");
-        fStartBalance = toolkit.createText(container, "");
-        toolkit.createLabel(container, "Minimal Balance:");
-        fMinBalance = toolkit.createText(container, "");
-        toolkit.createLabel(container, "Comment:");
-        fComment = toolkit.createText(container, "");
-
-        getSection().setClient(container);
-        toolkit.paintBordersFor(container);
-        refresh();
-    }
-
-    /* (non-Javadoc)
-     * @see org.eclipse.ui.forms.IFormPart#refresh()
-     */
-    public void refresh() {    	// When this page was added to the 'pages' extension point,    	// the extendable-object-id was set to net.sf.jmoney.bankAccount.    	// This indicates that this page should only appear when    	// the object selected in the navigation tree is an    	// object of that type.  We can therefore safely cast    	// the selected object to BankAccount.        BankAccount account = (BankAccount)fPage.fEditor.getSelectedObject();
-
-        setText(fName, account.getName());
-        setText(fCurrency, account.getCurrencyCode());
-        setText(fStartBalance, "" + account.getStartBalance());        setText(fComment, account.getComment());        BankAccount bankAccount = (BankAccount)account;	setText(fBank, bankAccount.getBank());
-        setText(fAccountNumber, bankAccount.getAccountNumber());
-        setText(fMinBalance, bankAccount.getMinBalance());
-        super.refresh();
-    }
-
-    protected void setText(Text text, Object value) {
-        text.setText(value == null? "": value.toString());
-    }
-
-}
