8d30b22712260fdabfc8bc6127d6f8684c6b1719
westbury <westbury@b97006a3-7e3d-0410-986d-ea81e883d316>
2006-04-24 03:21:21 +0000

    reorganize imports
    git-svn-id: https://jmoney.svn.sf.net/svnroot/jmoney/trunk@415 b97006a3-7e3d-0410-986d-ea81e883d316

--- net.sf.jmoney.currencypage/src/net/sf/jmoney/currencypage/CurrencyPage.java
+++ net.sf.jmoney.currencypage/src/net/sf/jmoney/currencypage/CurrencyPage.java
@@ -61,6 +61,7 @@ import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.action.Separator;
 import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.DialogPage;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.jface.viewers.ColumnWeightData;
