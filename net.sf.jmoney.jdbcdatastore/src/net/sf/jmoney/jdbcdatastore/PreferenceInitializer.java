63883fbfc7f581eafbc378ec2640cf4297ec5b17
westbury <westbury@b97006a3-7e3d-0410-986d-ea81e883d316>
2007-05-29 21:54:46 +0000

    put back JDBC connection defaults that were accidently overwritten
    git-svn-id: https://jmoney.svn.sf.net/svnroot/jmoney/trunk@532 b97006a3-7e3d-0410-986d-ea81e883d316

--- net.sf.jmoney.jdbcdatastore/src/net/sf/jmoney/jdbcdatastore/PreferenceInitializer.java
+++ net.sf.jmoney.jdbcdatastore/src/net/sf/jmoney/jdbcdatastore/PreferenceInitializer.java
@@ -26,20 +26,15 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
 		 * are good default values because there is no database setup and so
 		 * this plug-in will work 'out-of-the-box'.
 		 */
-/*		
 		store.setDefault("driver", "org.hsqldb.jdbcDriver");
 		store.setDefault("subProtocol", "hsqldb");
 		store.setDefault("subProtocolData", "file:accounts");
-//		store.setDefault("subProtocolData", "hsql://localhost/accounts");
 		store.setDefault("user", "sa");
 		store.setDefault("password", "");
-*/		
-				// Values for MS SQL Express
-				store.setDefault("driver", "net.sourceforge.jtds.jdbc.Driver");
-				store.setDefault("subProtocol", "jtds");
-				store.setDefault("subProtocolData", "sqlserver://localhost/jmoneyaccounts;instance=SQLEXPRESS");
-				store.setDefault("user", "sa");
-				store.setDefault("password", "secret");
-			
+		
+		// Use this value for subProtocolData instead if you
+		// want to use a database that is not in-process, allowing you
+		// to inspect the database while debugging.
+//		store.setDefault("subProtocolData", "hsql://localhost/accounts");
 	}
 }
