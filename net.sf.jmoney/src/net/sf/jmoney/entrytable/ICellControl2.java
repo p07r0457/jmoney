e69712fbdcd78cf8c4cd84960c3b10ac45e10d00
westbury <westbury@b97006a3-7e3d-0410-986d-ea81e883d316>
2008-10-18 16:51:38 +0000

    add new files that I forgot to add to previous commit
    git-svn-id: https://jmoney.svn.sf.net/svnroot/jmoney/trunk@724 b97006a3-7e3d-0410-986d-ea81e883d316

---
+++ net.sf.jmoney/src/net/sf/jmoney/entrytable/ICellControl2.java
@@ -0,0 +1,31 @@
+package net.sf.jmoney.entrytable;
+
+/**
+ * Interface implemented by all editable cells.
+ * <P>
+ * ICellControl objects may be controls representing single
+ * properties that can be edited by the user, or they may be
+ * 'composite' or 'container' cells.  If the former then the
+ * cell should implement this extended interface.  This interface
+ * allows the framework to tell the cell to show itself as being
+ * the selected cell or to be an unselected cell.  Normally a selected
+ * cell is white.
+ *
+ * @param <T>
+ */
+public interface ICellControl2<T> extends ICellControl<T> {
+
+	/**
+	 * Set this cell as the cell with the focus.
+	 * <P>
+	 * This usually just involves setting the color.
+	 */
+	void setSelected();
+
+	/**
+	 * Set this cell back to an un-selected state.
+	 * <P>
+	 * This usually just involves setting the color.
+	 */
+	void setUnselected();
+}
