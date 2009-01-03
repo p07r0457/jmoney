fde3fc7fa8edaf2a3297f9f1b10ced6db219ca4b
westbury <westbury@b97006a3-7e3d-0410-986d-ea81e883d316>
2009-01-03 19:53:06 +0000

    comments only
    git-svn-id: https://jmoney.svn.sf.net/svnroot/jmoney/trunk@892 b97006a3-7e3d-0410-986d-ea81e883d316

--- net.sf.jmoney/src/net/sf/jmoney/model2/ListKey.java
+++ net.sf.jmoney/src/net/sf/jmoney/model2/ListKey.java
@@ -2,6 +2,17 @@ package net.sf.jmoney.model2;
 
 import org.eclipse.core.runtime.Assert;
 
+/**
+ * This class acts as a key to a list of objects.  It allows an object
+ * to contain a reference to its parent without the need to instantiate
+ * the parent.
+ * <P>
+ * If the accounts are stored in a serialized file such as an XML file then
+ * all objects are instantiated anyway, but if the accounts are kept in
+ * a database then we don't always instantiate objects unless they are needed.
+ *
+ * @param <E> the class of objects kept in the list
+ */
 public class ListKey<E extends ExtendableObject> {
 	private IObjectKey parentKey;
 	private ListPropertyAccessor<E> listProperty;
