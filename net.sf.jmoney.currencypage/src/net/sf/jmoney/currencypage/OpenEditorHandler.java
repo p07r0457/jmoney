bd281dc363974fc7c23bf1960b751d4e0ed8dc88
westbury <westbury@b97006a3-7e3d-0410-986d-ea81e883d316>
2009-01-20 04:59:47 +0000

    fix workbench persistence
    git-svn-id: https://jmoney.svn.sf.net/svnroot/jmoney/trunk@900 b97006a3-7e3d-0410-986d-ea81e883d316

--- net.sf.jmoney.currencypage/src/net/sf/jmoney/currencypage/OpenEditorHandler.java
+++ net.sf.jmoney.currencypage/src/net/sf/jmoney/currencypage/OpenEditorHandler.java
@@ -7,6 +7,7 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.handlers.HandlerUtil;
@@ -16,12 +17,17 @@ public class OpenEditorHandler extends AbstractHandler {
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		try {
 			IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-			IEditorInput editorInput = new SessionEditorInput(JMoneyPlugin.getDefault().getSession(), null);
-			window.getActivePage().openEditor(editorInput, CurrencyEditor.ID);
+			IEditorInput editorInput = new SessionEditorInput();
+			
+			/**
+			 * Note that this extended form of openEditor is used because we must match on the editor id,
+			 * not the input id.  The form that takes just the first two parameters will match on the input
+			 * which is no good because the same input may be used for multiple editors.
+			 */
+			window.getActivePage().openEditor(editorInput, CurrencyEditor.ID, true, IWorkbenchPage.MATCH_ID);
 		} catch (PartInitException e) {
 			JMoneyPlugin.log(e);
 		}
 		return null;
 	}
-
 }
